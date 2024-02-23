1435631f5041dc46ede97570840543d9ca7e7e5c
MattKonop <mattsterprogrammer@gmail.com>
2024-02-22 17:10:13 -0800

    finished login through DB with Router. Co-authored-by: KevinCan <kevincan321@gmail.com> Co-authored-by: AllisonScott <allisonbscott97@gmail.com> Co-authored-by: AdamKim <adamkim1006@gmail.com>

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const cors = require('cors');
 
 
 
-//Allowing all IP Addresses via Atalas, need to change settings in Atlas for this to happen
+//Allowing all IP Addresses via Atlas, need to change settings in Atlas for this to happen
 const connectionString = 'mongodb+srv://solo:thisisdumb75@cluster0.6zuzqbm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';//will need mongoose connect function
 
 mongoose.connect(connectionString, {
@@ -28,15 +28,16 @@ mongoose.connect(connectionString, {
 app.use(session({
     secret: 'thisisacoolapp', //used to create hash to sign session ID cookie. required
     resave: false, //set based off warning from terminal. set to true will save to server and can affect performance
+    saveUninitialized: false,
     store: MongoStore.create({
         mongoUrl: connectionString,
         collection: 'mySessionCollection'}),//using connect-mongo as a session store instead of MemoryStore
     cookie:{
-        expires: 60000,
+        maxAge: 6000,
         sameSite: 'strict', //same site enforcement, not sure if we need it
     }
 }));
-app.use(cors());app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -45,12 +46,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/build', express.static(path.join(__dirname, '../build')));
 
 
-// app.use('/map/api/:stateName', (req, res, next) => {
-//     console.log('Request received for /map:', req.params);
-//     const {stateName} = req.params;
-//     req.stateName = stateName;
-//     return next();
-// }, fetchRouter);
+app.use('/map/api/:stateName', (req, res, next) => {
+    console.log('Request received for /map:', req.params);
+    const {stateName} = req.params;
+    req.stateName = stateName;
+    return next();
+}, fetchRouter);
 
 //any requests will head to loginSystem first to see if it matches any of those requests
 app.use('/', userRouter);
@@ -64,6 +65,7 @@ app.get('*', (req, res) => {
 app.post('/signup', userController.createUser, (req, res) => {
     return res.status(200).json({user: res.locals.newUser});
 })
+
 
 // Logout route
 app.post('/logout', (req, res) => {
